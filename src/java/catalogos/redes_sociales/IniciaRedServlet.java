5cf9d31f5a9e94724e56e72a9e9af9695a87a1fa
Maria Elena <maria@maria-HP-Mini-110-1000.(none)>
2012-03-09 20:59:01 -0600

    más cambios a redes, json

--- src/java/catalogos/redes_sociales/IniciaRedServlet.java
+++ src/java/catalogos/redes_sociales/IniciaRedServlet.java
@@ -12,7 +12,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.reflect.TypeToken;
+import java.io.BufferedReader;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
 /**
  *
  * @author ulises
@@ -34,7 +40,16 @@ public class IniciaRedServlet extends HttpServlet {
     response.setContentType("text/html;charset=UTF-8");
     PrintWriter out = response.getWriter();
     System.out.println("En el servlet de inicia red");
-    
+    String json = "{\"red\":[{\"elemento\": \"1\"}]}";
+    Type listType = new TypeToken<List<red>>(){}.getType();
+    Gson gson = new Gson();
+    List<red> redes = gson.fromJson(json,listType);
+    for (red r : redes){
+    System.out.println(r);
+    }
+   // Red obj =  gson.fromJson(json,Red.class);
+   //  System.out.println("Esto es lo que sale " + obj);
+	
     HttpSession session = request.getSession();
     Connection con = (Connection) session.getAttribute("conn");
     RedesSocialesDAO rsDAO = new RedesSocialesDAO(con);
@@ -93,4 +108,10 @@ public class IniciaRedServlet extends HttpServlet {
   public String getServletInfo() {
     return "Short description";
   }// </editor-fold>
+}
+
+class red{
+  public String elemento;
+  
+ 
 }
