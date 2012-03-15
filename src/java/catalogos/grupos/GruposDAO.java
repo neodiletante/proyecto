9a62e30913cf6bb974cd3b2a4b17833e91425e2b
Ulises Mendoza <garbage_collector@ymail.com>
2012-03-15 17:28:07 -0600

    cambio en impor fe prepared statement

--- src/java/catalogos/grupos/GruposDAO.java
+++ src/java/catalogos/grupos/GruposDAO.java
@@ -4,11 +4,8 @@
  */
 package catalogos.grupos;
 
-import com.mysql.jdbc.PreparedStatement;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
